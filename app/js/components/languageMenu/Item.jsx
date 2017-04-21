de17996d604e467e9a876b334494438fad169641
Martin Nikl <martin.nikl@DPL-PC0272.saholding.sa.priv>
2017-04-21 17:29:13 +0200

    - icon of active vocabulary has now different color

--- app/js/components/languageMenu/Item.jsx
+++ app/js/components/languageMenu/Item.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { vocabularyLang } from '../../userState'
 
 class Item extends React.Component {
   render() {
+    const activeClassName = this.props.name === vocabularyLang ? 'btn-success' : 'btn-info'
     return(
-      <li onClick={this.props.handleChange} class="btn bg-info btn-sm" data-lang={this.props.name}>{this.props.name}</li>
+      <li onClick={this.props.handleChange} class={`btn ${activeClassName} btn-sm`} data-lang={this.props.name}>{this.props.name}</li>
     )
   }
 }
